d90e6904aa448c994a9360432982d8a23faff493
Michael Napolitano <mnapolit@cisco.com>
2016-10-11 22:08:12 -0400

    Initial Commit

---
+++ MooTorrent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MooTorrent
+//
+//  Created by mnapolit on 10/11/16.
+//  Copyright © 2016 Micmoo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
